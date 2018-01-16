56adbe64764abe8b58ba3a09e4870aed049b52f7
Mikhail <mikesmnx@gmail.com>
2018-01-16 23:32:11 +0300

    phrasal verbs

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
-import About from '../about'
+import PhrasalVerbs from '../phrasal-verbs'
 
 const App = () => (
   <div>
     <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
+      <Link to="/">Home</Link> &nbsp;
+      <Link to="/phrasal-verbs">Phrasal Verbs</Link> &nbsp;
+      <Link to="/phrasal-verbs/sentences">Sentences</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
+      <Route exact path="/phrasal-verbs" component={PhrasalVerbs} />
+      <Route exact path="/phrasal-verbs/sentences" component={PhrasalVerbs} />
     </main>
   </div>
 )
---
+++ src/containers/phrasal-verbs/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div className="container">
+    <h1>Phrasal Verbs</h1>
+    
+  </div>
+)
